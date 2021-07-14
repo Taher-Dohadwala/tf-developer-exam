2713aaac2ffcc7eff4a8b2cc69022e807bc75969
Taher Dohadwala <taher.dohadwala1@gmail.com>
2021-07-14 16:33:53 -0500

    example 4 data added

--- c1e4.py
+++ c1e4.py
@@ -4,3 +4,22 @@ Create a convolutional neural network that trains to 100% accuracy on these imag
 
 Hint -- it will work best with 3 convolutional layers.+
 """
+import tensorflow as tf
+
+DESIRED_ACCURACY = 0.999
+
+# Define model
+model = tf.keras.Sequential([
+
+
+
+
+
+])
+# Load data
+
+# Callbacks
+
+# Compile
+
+# Fit
